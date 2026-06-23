178a404a491e95cf7465c5c1e3e949bd09d3418c
akhila1324 <kondeboyinaakhila1324@gmail.com>
2026-06-23 14:57:48 +0530

    Add files via upload

---
+++ StringOperations (1).java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class StringOperations {
+    public static boolean isPalindrome(String str) {
+        str = str.toLowerCase();
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+    public static int countVowels(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        if (isPalindrome(input)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        int vowels = countVowels(input);
+        System.out.println("Number of vowels: " + vowels);
+
+        scanner.close();
+    }
+}
